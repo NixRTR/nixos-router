e28eb2e5a778e6f99f2f51a4159e7aac363ef928
beardedtek <kenny@beardedtek.com>
2025-11-08 21:17:26 -0900

    Add experimental Nix features for improved functionality in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -64,6 +64,11 @@ in
       ./router.nix
     ];
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
